a6405723432c0ec4cc375350c5d8929502f34fcd
attilagyongyosi <attilagyongyosi0@gmail.com>
2021-06-13 15:17:12 +0100

    feat: including ESLint and path-linter for code quality

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        '@attilagyongyosi/eslint-config',
+    ],
+    env: {
+        node: true
+    },
+    parserOptions: {
+        ecmaVersion: 11
+    }
+}
