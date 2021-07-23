32fe60600209a208be1171cd92580fca16a1d674
Adam Nyitrai <adam.nyitrai87@gmail.com>
2021-07-23 20:30:56 +0100

    feat: registration form connected to the server

--- src/auth/user.schema.js
+++ src/auth/user.schema.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     userName: { type: String, required: true },
     password: { type: String, required: true },
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    gender: String,
+    dateOfBirth: Date,
+    joinReason: String
 });
 
 module.exports = userSchema;
