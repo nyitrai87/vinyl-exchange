929a961e934809f94574d2076f9c592d1c865745
Adam Nyitrai <adam.nyitrai87@gmail.com>
2021-08-12 17:36:52 +0100

    refactor: refactored code by request

--- src/public/home.js
+++ src/public/home.js
@@ -2,25 +2,27 @@ async function onContentLoaded() {
     const response = await fetch('/vinyls');
     const vinyls = await response.json();
 
-    vinyls.forEach(element => {
-        const div = document.createElement('div');
-        const strong = document.createElement('strong');
-        const p = document.createElement('p');
-        const img = document.createElement('img');
-        const main = document.getElementById('main');
-        const title = document.createTextNode(element.title);
-        const artist = document.createTextNode(element.artist);
-        div.classList.add('vinyl', 'flex', 'flex-column', 'panel');
-        img.classList.add('cover');
-        img.src = 'placeholder.jpg';
-        img.alt = 'Cover placeholder';
-        div.appendChild(strong);
-        strong.appendChild(title);
-        div.appendChild(p);
-        p.appendChild(artist);
-        div.appendChild(img);
-        main.appendChild(div);
+    vinyls.forEach(vinyl => {
+        insertAlbumPanel(vinyl);
     });
+}
+
+function insertAlbumPanel(vinyl) {
+    const div = document.createElement('div');
+    const strong = document.createElement('strong');
+    const p = document.createElement('p');
+    const img = document.createElement('img');
+    const main = document.getElementById('main');
+    strong.innerText = vinyl.title;
+    p.innerText = vinyl.artist;
+    div.classList.add('vinyl', 'flex', 'flex-column', 'panel');
+    img.classList.add('cover');
+    img.src = 'placeholder.jpg';
+    img.alt = 'Cover placeholder';
+    div.appendChild(strong);
+    div.appendChild(p);
+    div.appendChild(img);
+    main.appendChild(div);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
